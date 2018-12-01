2e78a1ace4fca1d3780ac0d87d7b98dfddd8a5b7
limhweechin <hcl2109@gmail.com>
2018-12-01 16:22:00 +0800

    End of Day 5 (01 Dec 2018)

---
+++ home.js
@@ -0,0 +1,33 @@
+var my_pets;
+var my_pets_age;
+var my_pets_birthday;
+
+my_pets="Maomao";
+my_pets_age=5;
+my_pets_birthday="22/08/2013";
+
+console.log(my_pets);
+console.log(my_pets_age);
+console.log(my_pets_birthday);
+
+var my_pets_favourite_toy="longlong";
+var my_pets_favourite_food;
+
+console.log(my_pets_favourite_food);
+console.log(typeof(my_pets));
+console.log(typeof(my_pets_age));
+
+var firstName;
+var lastName;
+
+firstName="Olivia"
+lastName="Ng"
+
+var fullName= firstName +" "+ lastName;
+console.log(fullName);
+
+var qty=2
+var total=200
+var qtyTotal=qty+total
+console.log(qtyTotal)
+
